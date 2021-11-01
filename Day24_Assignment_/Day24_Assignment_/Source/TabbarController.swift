11977702f2ce85426162e98679921a8078eb42c0
chansenbbai <chanchu@hansung.ac.kr>
2021-11-02 01:42:48 +0900

    [#1] 초기 세팅

---
+++ Day24_Assignment_/Day24_Assignment_/Source/TabbarController.swift
@@ -0,0 +1,29 @@
+//
+//  TabbarController.swift
+//  Day24_Assignment_
+//
+//  Created by 김승찬 on 2021/11/02.
+//
+
+import UIKit
+
+class TabbarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Day24_Assignment_/Day24_Assignment_/Source/ViewController/WriteViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WriteViewController.swift
+//  Day24_Assignment_
+//
+//  Created by 김승찬 on 2021/11/02.
+//
+
+import UIKit
+
+class WriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func dismissHome(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func touchSavedButton(_ sender: UIButton) {
+    }
+    
+}
