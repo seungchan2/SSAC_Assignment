fe49836ba99472a751c9cb31c4fe7d87abe40d2a
chansenbbai <chanchu@hansung.ac.kr>
2021-11-02 00:37:15 +0900

    initial commit

---
+++ Day24_Assignment_/Day24_Assignment_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day24_Assignment_
+//
+//  Created by 김승찬 on 2021/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
